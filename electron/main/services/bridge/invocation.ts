2a55325ddf7dcb511f415ec74911da34139616c6
Hydrogen <3038094028@qq.com>
2022-09-25 16:36:07 +0800

    refactor(bridge): remote method main

---
+++ electron/main/services/bridge/invocation.ts
@@ -0,0 +1,100 @@
+import { FutureReply } from "../../../../types/bridge";
+import { IpcMain, IpcMainEvent } from "electron";
+
+export async function toReply<T, E = any>(
+  id: number,
+  promise: Promise<T>,
+): Promise<FutureReply<T, E>> {
+  try {
+    const value = await promise;
+    return { id, error: false, value };
+  } catch (err: any) {
+    return { id, error: true, value: err };
+  }
+}
+
+export type FutureMethod<T extends [unknown] | unknown[], R, E> = (
+  id: number,
+  ...args: T
+) => Promise<FutureReply<R, E>>;
+
+export type Method<T extends [unknown] | unknown[], R> = (
+  ...args: T
+) => Promise<R>;
+
+export type MethodStore = Map<string, FutureMethod<any[], any, any>>;
+
+const kInstance = Symbol("kInstance");
+
+export class InvokeBridgeMain {
+  static [kInstance]: InvokeBridgeMain = new InvokeBridgeMain();
+
+  protected _methods: MethodStore = new Map();
+
+  private constructor() {}
+
+  setup(ipc: IpcMain) {
+    ipc.on("bridge:invoke_call", this._handleIpc.bind(this));
+  }
+
+  protected async _call(
+    id: number,
+    method: string,
+    args: any[],
+  ): Promise<FutureReply<any, any>> {
+    if (!this.has(method)) {
+      return toReply(id, Promise.reject<any>("error: not found method"));
+    }
+
+    const fn = this._methods.get(method);
+    try {
+      const value = await fn(id, ...args);
+      return value;
+    } catch (err: any) {
+      return toReply(id, Promise.reject<any>(err?.toString() ?? err));
+    }
+  }
+
+  protected async _handleIpc(event: IpcMainEvent, msg: any) {
+    if (
+      typeof msg != "object" || typeof msg.id != "number" ||
+      typeof msg.method != "string" || !Array.isArray(msg.params)
+    ) {
+      return;
+    }
+
+    const reply = await this._call(msg.id, msg.method, msg.params);
+    event.sender.send("bridge:invoke_reply", reply);
+  }
+
+  handleRaw<T extends [unknown] | unknown[], R, E>(
+    method: string,
+    handler: FutureMethod<T, R, E>,
+  ): this {
+    this._methods.set(method, handler);
+    return this;
+  }
+
+  handle<T extends [unknown] | unknown[], R>(
+    method: string,
+    handler: Method<T, R>,
+  ): this {
+    this.handleRaw<T, R, any>(
+      method,
+      (id, ...args) => toReply(id, handler(...args)),
+    );
+    return this;
+  }
+
+  has(method: string) {
+    return this._methods.has(method);
+  }
+
+  remove(method: string) {
+    return this._methods.delete(method);
+  }
+}
+
+export const invokeBridge = InvokeBridgeMain[kInstance];
+
+export { FutureReply };
--- types/bridge.d.ts
+++ types/bridge.d.ts
@@ -15,4 +15,48 @@ interface BridgeReply {
   payload: any;
 }
 
-export { ObservableBridgeUpdate, BridgeRequest, BridgeReply };
+export { BridgeReply, BridgeRequest, ObservableBridgeUpdate };
+
+export interface Invocation<
+  T extends [unknown] | unknown[],
+  K extends string = string,
+> {
+  id: number;
+  method: K;
+  params: T;
+}
+
+export interface RejectedReply<E = any> {
+  id: number;
+  error: true;
+  value: E;
+}
+
+export interface FulfilledReply<T> {
+  id: number;
+  error: false;
+  value: T;
+}
+
+export type FutureReply<T, E = any> = FulfilledReply<T> | RejectedReply<E>;
+
+export interface ObservableCompleteReply {
+  id: number;
+  type: 0;
+}
+export interface ObservableNextReply<T> {
+  id: number;
+  type: 1;
+  value: T;
+}
+
+export interface ObservableErrorReply<E> {
+  id: number;
+  type: 2;
+  value: E;
+}
+
+export type ObservableReply<T, E> =
+  | ObservableNextReply<T>
+  | ObservableErrorReply<E>
+  | ObservableCompleteReply;
